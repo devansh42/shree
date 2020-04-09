acc83e1d1c76c01646857a6d649713f8a27d2ef6
Devansh Gupta <devanshguptamrt@gmail.com>
2020-04-09 08:33:04 +0000

    some bug fixed

--- do.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install -y docker.io
-git clone -b test https://github.com/devansh42/shree.git
-cd shree
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
