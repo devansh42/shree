8b2df176272c48c2667b9c3d658fea2ff96c4f19
Devansh Gupta <devanshguptamrt@gmail.com>
2020-04-09 23:27:25 +0530

    testing completed

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#This files build docker images and push them to docker hub
-docker build --no-cache -t devansh42/shree-backend ./back
-docker build --no-cache	 -t devansh42/shree-ca ./ca
-docker build  --no-cahce  -t devansh42/shree-serv ./exe/serv
